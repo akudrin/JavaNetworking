ee15791a41db668828452e2391d5679862e63588
Andrei Kudrin <andreikudrin@gmail.com>
2019-04-14 16:05:11 +0100

    chat server/client app

---
+++ src/com/akudrin/chatapp/ChatClient.java
@@ -0,0 +1,35 @@
+package com.akudrin.chatapp;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+
+public class ChatClient {
+	public ChatClient() {
+		SocketAddress address = new InetSocketAddress("127.0.0.1", 5000);
+		try (SocketChannel socketChannel = SocketChannel.open(address)) {
+			System.out.println("Connected to Chat Server");
+			String message;
+			Scanner scanner = new Scanner(System.in);
+			while (true) {
+				System.out.println("Waiting for message from the server…");
+				System.out.println("Message: " + HelperMethods.receiveFixedLengthMessage(socketChannel));
+				System.out.print("> ");
+				message = scanner.nextLine();
+				if (message.equalsIgnoreCase("quit")) {
+					HelperMethods.sendFixedLengthMessage(socketChannel, "Client terminating");
+					break;
+				}
+				HelperMethods.sendFixedLengthMessage(socketChannel, message);
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		new ChatClient();
+	}
+}
